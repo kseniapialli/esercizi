e7cbe12d4bb79bef402eee0b8eac1752a55761cc
kseniapialli <ksenia.pialli@campusdavinci.edu.it>
2025-12-02 11:31:08 +0100

    esercizio maggiore pari dispari

---
+++ esercizio_7.py
@@ -0,0 +1,27 @@
+#scrivere una funzione che dati due numeri iteri restituisca il maggiore
+def nMaggiore (nmero1,numero2):
+    if numero1>numero2:
+        return numero1
+    else:
+        return numero2
+    
+#scrivere una procedura che dato un numero intero stampi a video se pari o dispari
+def nPariDispari (numero1):
+    if numero1%2==0:
+        print("il numero è pari")
+    else:
+        print("il numero è dispari")
+
+uno=input("inserire il primo numero ")
+uno=int(uno)
+due=input("inserire il secondo numero ")
+due=int(due)
+
+risultato=nMaggiore(numero1,numero2)
+print(risultato)
+nPariDispari(risultato)
+
+#con questo esercizio ho scritto un programma che dati due numeri interi stampi a video se il maggiore è pari o dispari
+
+#risultato2=nMaggiore(uno) non funziona pervhe servono due parametri
+#nPariDispari(uno, due) non funziona perche serve solo un parametro
